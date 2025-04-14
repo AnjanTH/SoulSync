626c876945cac12552ced50b2cfe88bbb7123247
AnjanTH <anjanth2552004@gmail.com>
2025-04-14 13:03:42 +0530

    change

--- frontend/components/ChatInterface.js
+++ frontend/components/ChatInterface.js
@@ -193,6 +193,7 @@ export default function ChatInterface({ isSidebarOpen, toggleSidebar }) {
             messages={messages}
             onSelectChat={setSelectedDate}
             selectedDate={selectedDate}
+            
           />
         </div>
       </div>
